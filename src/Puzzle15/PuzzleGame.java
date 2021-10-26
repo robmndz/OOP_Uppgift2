0c19b501e8bf7f31d5e6cd652bb35dc1eaffe42f
Roberto Mendez <roberto.mendez1983@gmail.com>
2021-10-26 18:36:04 +0200

    Creating tiles

--- src/Puzzle15/PuzzleGame.java
+++ src/Puzzle15/PuzzleGame.java
@@ -31,7 +31,21 @@ public class PuzzleGame implements ActionListener {
     }
 
     void createTiles() {
+        panel.setLayout((new GridLayout(4, 4)));
+        buttons = new JButton[16];
+        for (int i = 0; i < buttons.length - 1; i++) {
+            buttons[i] = new JButton("" + (i + 1));
+            buttons[i].addActionListener(this);
+            panel.add(buttons[i]);
+        }
 
+        buttons[buttons.length - 1] = new JButton();
+        buttons[buttons.length - 1].setOpaque(false);
+        buttons[buttons.length - 1].setContentAreaFilled(false);
+        buttons[buttons.length - 1].setBorderPainted(false);
+
+        buttons[buttons.length - 1].addActionListener(this);
+        panel.add(buttons[buttons.length - 1]);
     }
 
 
