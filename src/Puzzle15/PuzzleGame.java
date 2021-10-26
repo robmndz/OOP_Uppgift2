52adeff984fc131d578fcc79f963a8a40da2d3f4
Roberto Mendez <roberto.mendez1983@gmail.com>
2021-10-26 18:25:14 +0200

    adding all buttons to the frame + actionListener

--- src/Puzzle15/PuzzleGame.java
+++ src/Puzzle15/PuzzleGame.java
@@ -1,4 +1,51 @@
 package Puzzle15;
 
-public class PuzzleGame {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PuzzleGame implements ActionListener {
+
+    JFrame frame;
+    JButton startButton;
+    JPanel panel;
+    int indexEmptyButton = 15;
+    JButton[] buttons;
+
+    public PuzzleGame() {
+        frame = new JFrame("Puzzle");
+        panel = new JPanel();
+
+        // adding all buttons to the frame & actionListener
+        startButton.add(new JButton("New Game"));   // Start button
+        startButton.addActionListener(this);
+
+        frame.add(startButton, BorderLayout.NORTH);
+        frame.add(panel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
